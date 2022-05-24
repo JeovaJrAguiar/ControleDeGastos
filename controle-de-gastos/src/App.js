6e07d0be63568f8ecbb541cb245b225c0927fe0d
Aguiar <64222622+JeovaJrAguiar@users.noreply.github.com>
2022-05-24 12:46:30 -0300

    .

--- controle-de-gastos/src/App.js
+++ controle-de-gastos/src/App.js
@@ -1,28 +1,22 @@
+import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <div  className = 'Rodape'>
-          Testando Rodape
-        </div>
-        <div className='Titulo'>
-            <p>
-              <h1>Controlhe de Gastos</h1>
-            </p>
-        </div>
-        <footer className='Rodape'>
-          <a
-              className="App-link"
-              href="https://github.com/JeovaJrAguiar/ControleDeGastos"
-              target="_blank"
-              //rel="noopener noreferrer"
-            >
-              GitHub
-          </a>
-        </footer>
-        
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
       </header>
     </div>
   );
